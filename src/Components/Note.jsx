e78f55406595910a01983715afebfe2114585f99
ikrama10 <ikramakhan123@gmail.com>
2023-12-20 18:30:44 +0500

    sjcbsj

---
+++ src/Components/Note.jsx
@@ -0,0 +1,84 @@
+import React, { useEffect, useState } from "react";
+import {
+  faPenToSquare,
+  faTrashCan,
+  faCheck,
+} from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
+const Note = ({ id, note, setFetcheNotes, fetchedNotes }) => {
+  const [isEditing, setIsEditing] = useState(false);
+  const [newNote, setNewNote] = useState("");
+
+  const handleEdit = () => {
+    setIsEditing(true);
+  };
+
+  const handleSave = (noteId) => {
+    setIsEditing(false);
+    setFetcheNotes((prevVal) => {
+      return prevVal.map((note, index) => {
+        if (noteId === index + 1) {
+          return newNote;
+        } else {
+          return note;
+        }
+      });
+    });
+  };
+
+  useEffect(() => {
+    setNewNote(note);
+  }, [note]);
+
+  const handleDel = (noteId) => {
+    const updatedNote = fetchedNotes.filter(
+      (note, index) => noteId !== index + 1
+    );
+    setFetcheNotes(updatedNote);
+  };
+
+  return (
+    <>
+      <div
+         className=" min-h-[380px] w-[30%] bg-white box-border rounded-lg shadow-lg overflow-hidden mt-10 "
+      >
+        <div className="bg-[#52ab98] p-3 text-white font-semibold flex justify-end">
+
+          <div>
+            {isEditing ? (
+              <FontAwesomeIcon
+                icon={faPenToSquare}
+                className="text-white h-5 cursor-pointer"
+                onClick={() => handleSave(id)}
+              />
+            ) : (
+              <FontAwesomeIcon
+                icon={faPenToSquare}
+                className="text-white h-5 cursor-pointer"
+                onClick={handleEdit}
+              />
+            )}
+
+            <FontAwesomeIcon
+              icon={faTrashCan}
+              className="text-white h-5 ml-4 cursor-pointer"
+              onClick={() => handleDel(id)}
+            />
+          </div>
+        </div>
+        {isEditing ? (
+          <textarea
+            className="resize-none h-full w-full outline-none p-3 text-2xl"
+            value={newNote}
+            onChange={(e) => setNewNote(e.target.value)}
+          ></textarea>
+        ) : (
+          <h3 className="p-3 text-base">{newNote}</h3>
+        )}
+      </div>
+    </>
+  );
+};
+
+export default Note;
---
+++ src/Components/NotesCrud.jsx
@@ -0,0 +1,63 @@
+import React, { useEffect, useLayoutEffect, useState } from "react";
+import Note from "./Note";
+
+const handleGetNotes = () => {
+  let list = localStorage.getItem("note");
+
+  if (list) {
+    return JSON.parse(localStorage.getItem("note"));
+  } else {
+    return [];
+  }
+};
+
+const NotesCrud = () => {
+  const [fetchedNotes, setFetcheNotes] = useState(handleGetNotes());
+
+  const handleAddNote = () => {
+    const note = "";
+    setFetcheNotes((prevVal) => [...prevVal, note]);
+    console.log(fetchedNotes);
+  };
+
+  useEffect(() => {
+    localStorage.setItem("notes", JSON.stringify(fetchedNotes));
+  }, [fetchedNotes]);
+
+ 
+  useLayoutEffect(() => {
+    document.body.style.backgroundColor = "#c8d8e4"
+});
+
+  return (
+    <div className="h-screen">
+       <h1 className="text-2xl  italic font-bold uppercase text-center p-4 bg-[#52ab98] text-white  font-serif">
+           Notes App
+        </h1>
+      <div className="flex flex-col pt-4  items-center justify-center">
+        <button
+          className="bg-[#52ab98] text-white px-6 py-2 rounded-md my-6 hover:bg-[#31665b ] shadow-lg font-semibold cursor-pointer transition-all duration-100"
+          onClick={handleAddNote}
+        >
+          + Add note
+        </button>
+      </div>
+
+      <div className="flex flex-wrap justify-between w-[90%] mx-auto">
+        {fetchedNotes.map((note, index) => {
+          return (
+            <Note
+              key={index}
+              id={index + 1}
+              note={note}
+              setFetcheNotes={setFetcheNotes}
+              fetchedNotes={fetchedNotes}
+            />
+          );
+        })}
+      </div>
+    </div>
+  );
+};
+
+export default NotesCrud;
